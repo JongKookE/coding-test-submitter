01a3ea193cfb84d43fd40d87fe5d11a7c5847e00
박종국 <whdrnrdl789@naver.com>
2025-03-28 00:44:04 +0900

    refactor: remove debuggin console and util.js

--- scripts/baekjoon.js
+++ scripts/baekjoon.js
@@ -6,17 +6,28 @@ document.addEventListener(
     const pastedData = clipboardData.getData("text");
 
     const formattedData = toBaekjoon(textToken(pastedData));
-    console.log(formattedData);
     document.execCommand("insertText", false, formattedData);
     // paste 이벤트는 cancleable하지 않기 때문에 cancelable 파라미터를 true로 지정해줘야함
   },
   true
 );
 
-function toBaekjoon(tokens) {
+const toBaekjoon = (tokens) => {
   let startPoint = getStartIndex(tokens);
   let classNameIndex = tokens.indexOf("public") + 4;
 
   tokens[classNameIndex] = "Main";
   return tokens.slice(startPoint).join("");
-}
+};
+
+const getStartIndex = (tokens) => {
+  if (tokens[0] === "package") return 4;
+  return 0;
+};
+
+const textToken = (text) => {
+  // 공백, 줄바꿈, 탭을 기준으로 분리하는 정규표현식
+  // ()을 통해서 분리되는 구분자도 배열에 포함시킴
+  textArray = text.split(/(\s+)/);
+  return textArray;
+};
--- scripts/programmers.js
+++ scripts/programmers.js
@@ -6,7 +6,7 @@ document.addEventListener(
     const pastedData = clipboardData.getData("text");
 
     const formattedData = toProgrammers(textToken(pastedData));
-    // console.log(formattedData);
+
     document.execCommand("insertText", false, formattedData);
     // paste 이벤트는 cancleable하지 않기 때문에 cancelable 파라미터를 true로 지정해줘야함
   },
@@ -14,30 +14,38 @@ document.addEventListener(
 );
 
 function toProgrammers(tokens) {
-    let startPoint = getStartIndex(tokens);
-    tokens.splice(0, startPoint);
+  let startPoint = getStartIndex(tokens);
+  tokens.splice(0, startPoint);
 
-    const removeStartIndex = tokens.indexOf("public");
-    const removeEndIndex = removeClassBlockEndIndex(tokens);
+  const removeStartIndex = tokens.indexOf("public");
+  const removeEndIndex = removeClassBlockEndIndex(tokens);
 
-    console.log(tokens.splice(removeStartIndex, removeEndIndex-removeStartIndex+4).join(''));
-    tokens.splice(tokens.lastIndexOf('}'));
-    return tokens.join("");
+  tokens.splice(removeStartIndex, removeEndIndex - removeStartIndex + 4).join("");
+  tokens.splice(tokens.lastIndexOf("}"));
+  return tokens.join("");
 }
 
+function removeClassBlockEndIndex(tokens) {
+  let start = tokens.indexOf("void") + 6;
 
-function removeClassBlockEndIndex(tokens){
-    let start = tokens.indexOf('void') + 6;
-    
-    const stack = [];
+  const stack = [];
 
-    for(var i = start; i < tokens.length; i++){
-        const token = tokens[i];
-        // console.log(token);
-        if(token.includes('{')) stack.push('{');
-        else if (token.includes('}')) stack.pop();
-        // console.log(stack);
-        if(stack.length === 0) return i;
-    }
-    
+  for (var i = start; i < tokens.length; i++) {
+    const token = tokens[i];
+    if (token.includes("{")) stack.push("{");
+    else if (token.includes("}")) stack.pop();
+    if (stack.length === 0) return i;
+  }
 }
+
+const getStartIndex = (tokens) => {
+  if (tokens[0] === "package") return 4;
+  return 0;
+};
+
+const textToken = (text) => {
+  // 공백, 줄바꿈, 탭을 기준으로 분리하는 정규표현식
+  // ()을 통해서 분리되는 구분자도 배열에 포함시킴
+  textArray = text.split(/(\s+)/);
+  return textArray;
+};
