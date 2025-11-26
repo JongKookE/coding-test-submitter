aecc57619f0c4f3758684b8dd091362f25b16332
박종국 <whdrnrdl789@naver.com>
2025-11-26 22:50:59 +0900

    fix: seperate className and filename

--- popup/index.js
+++ popup/index.js
@@ -2,46 +2,70 @@ const myAlgorithmPath = "C:\\jongkookE\\Algorithm\\src"
 console.log("Popup script loaded");
 
 document.getElementById('image-text-button').addEventListener('click', async () => {
-    console.log("Button Clicked!!")
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    console.log(tab)
-    const filename = "BOJ_" + tab.title.replace(/^(\d+)[^\w\s]+\s*(.*)/, '$1\_$2') + ".java";
-    console.log(filename)
-
-    downloadJavaFile(baekjoonFormat, filename)
+    console.log(tab.title)
+    if(tab.url.includes("acmicpc")){
+        const className = "BOJ_" + tab.title.replace(/^(\d+)[^\w\s]+\s*(.*)/, '$1\_$2').replace(/ /g, '_');
+        const filename = className +  ".java";
+        downloadJavaFile(className, makeBaekjoonFormat(filename))
+    }
+    else if(tab.url.includes("programmers")){
+        const className = "PRO_" + tab.title.replace(/^(\d+)[^\w\s]+\s*(.*)/, '$1\_$2').replace(/ /g, '_');
+        const filename = className + ".java"
+        console.log(filename)
+        downloadJavaFile(className, makeProFormat(filename))
+    }
 });
 
-const downloadJavaFile = (text, filename) => {
-    const blob = new Blob([text], { type: 'java/plain;charset=utf-8' });
-
-    // 다운로드 URL 생성
+const downloadJavaFile = (filename, format) => {
+    const blob = new Blob([format], { type: 'java/plain;charset=utf-8' });
     const url = URL.createObjectURL(blob);
 
-    // 다운로드 트리거
     chrome.downloads.download({
         url: url,
         filename: filename,
-        saveAs: true  // 사용자가 저장 위치를 선택할 수 있게 함
+        saveAs: true  
     }, (downloadId) => {
         if (chrome.runtime.lastError) {
             console.error("Download error:", chrome.runtime.lastError);
         } else {
             console.log("Download started:", downloadId);
         }
-        // URL 해제
         URL.revokeObjectURL(url);
     });
 }
 
-const baekjoonFormat = `
-\\import java.io.*;
+const makeBaekjoonFormat = (filename) => {
+    return `
+import java.io.*;
 import java.util.*;
 
-public class Main {
+public class ${filename} {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // StringTokenizer st = new StringTokenizer(br.readLine());
-        // TODO: 코드 작성
+        // TODO: logic
     }
 };
 `
+}
+
+const makeProFormat = (filename) => {
+    return `
+import java.io.*;
+import java.util.*;
+
+public class ${filename} {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+    static class Solution{
+        int solution(){
+            answer = 0;
+            return answer;
+        }
+    }
+};
+`
+}
